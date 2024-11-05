a0f6f33bc383fd5fd60cee6ecbbd3cb2c3a8fb1d
SaadSikder1 <Saad.Sikder@laetottenham.ac.uk>
2024-11-05 10:20:58 +0000

    Added menu slection choices and sleep

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 cat_attributes = {
-    "intelligence": 0,
-    "energy": 0,
-    "weight": 0,
-    # change the inital values above
+    "intelligence": 10,
+    "energy": 50,
+    "weight": 10,
 }
 
 print("Welcome to my cat game!")
 
 # Take the user inputs for name and colour:
-name = input("Enter name:")
-# ...
+name = input("Enter the name of your cat: ")
+colour = input("Enter the colour of your cat: ")
 
 # start the while loop
 while True:
     # Finish the string below
     option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
 
-    if option == '1':
-        # change the cat's attributes here
-        pass
-    elif option == '2':
-        pass
-    # elif ...
+    if option == '1' and cat_attributes["energy"] > 5:
+        print("You play with your cat.")
+        cat_attributes["energy"] -= 5
+        cat_attributes["weight"] -=  - 0.8
+    elif option == '2' and cat_attributes["energy"] > 5:
+        print("You train your cat. ")
+        cat_attributes["intelligence"] += 5
+        cat_attributes["energy"] -= 5
+        cat_attributes["weight"] -= 0.6
+    elif option == '3':
+        print("Here is your cat's stats: ")
+        print("Your cat's energy currently is" + cat_attributes["energy"])
+        print("Your cat's intelligence currently is" + cat_attributes["intelligence"])
+        print("Your cat's weight currently is" + cat_attributes["weight"])
     else:
-        pass
+        print("It seems your cat does not have enough energy for these actions.")
+        print("Nothing happens.")
 
-    # finish off the if statements below
     if cat_attributes['energy'] < 0:
-        pass
-    # elif ...
+        print("Your cat runs out of energy, it drifts off into sleep ")
+        cat_attributes["energy"] += 40
